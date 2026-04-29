9e1a11c4bea5e413a05f06ac56c2d03f0a40a890
WangKang-Leo <kang_wang0822@hotmail.com>
2026-04-29 20:08:27 +0200

    update code

--- Figures/Multimodal_PREDIX_HER2_FigureS11.R
+++ Figures/Multimodal_PREDIX_HER2_FigureS11.R
@@ -1,16 +1,9 @@
-# FigS10b-d 
-##########Frequency of selected features##########
-library(data.table);library(ggplot2);library(forcats);library(tidyverse)
-All=fread("E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/Best_ML_Results_12_10_25/BestResults/Both_Integrate/all_no_norm_selfeats_10_run4_Feature_weights.csv")
-All=All[,-1];All$voting_percentage <- round(100*All$hybrid_ranks_ / sum(All$hybrid_ranks_),2)
-All$group=sub("_.*", "",All$feat_name);All=All[1:20,]
-DHP=fread("E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/Best_ML_Results_12_10_25/BestResults/Treat_0_Integrate/all_no_norm_TREAT_0_selfeats_10_run4_Feature_weights.csv")
-DHP=DHP[,-1];DHP$voting_percentage <- round(100*DHP$hybrid_ranks_ / sum(DHP$hybrid_ranks_),2)
-DHP$group=sub("_.*", "",DHP$feat_name);DHP=DHP[1:20,]
-TDM1=fread("E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/Best_ML_Results_12_10_25/BestResults/Treat_1_Integrate/all_no_norm_TREAT_1_selfeats_10_run4_Feature_weights.csv")
-TDM1=TDM1[,-1];TDM1$voting_percentage <- round(100*TDM1$hybrid_ranks_ / sum(TDM1$hybrid_ranks_),2)
-TDM1$group=sub("_.*", "",TDM1$feat_name);TDM1=TDM1[1:20,]
-
+# FigS10b
+# selected features for All
+library(data.table);library(ggplotify);library(ggpubr)
+df=fread("E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/consensus_signatures.csv") 
+df=df[df$Scenario=="Global",]
+df$Modality=factor(df$Modality,levels = c("Clin","DNA","RNA","Prot","WSI"))
 group_colors <- c(
   Clin = "#434279", 
   DNA = "#f2a104", 
@@ -18,132 +11,109 @@ group_colors <- c(
   Prot= "#9467bd",
   WSI = "#00743f"
 )
-#All
-All$feat_name=factor(All$feat_name,levels = c("RNA_mRNA-PGR","WSI_Cell_Interaction","RNA_Fatty_acid_metabolism", 
-                                              "RNA_mRNA-ESR1","Prot_CDK12","RNA_Glutathione_metabolism",
-                                              "DNA_RAB11FIP1_CNA","RNA_sspbc.subtype_Her2","DNA_RPL19_CNA",             
-                                              "Prot_SLC12A2","Prot_RPL19","Prot_ERBB2",                
-                                              "RNA_Glycolysis","Prot_MIEN1","RNA_HER2DX_HER2_amplicon",  
-                                              "Clin_ER","RNA_Treg","Prot_PPP1R1B",              
-                                              "RNA_Exosome","Prot_EEA1")) 
-All$feat_name <- fct_rev(All$feat_name)
-baseDir <- "E:/Projects/PREDIX_HER2/Multimodal/"
-source (paste0(baseDir,"/Code/theme.R"))
-figure_font_size=13
-All_feature=ggplot(data=All,aes(feat_name,voting_percentage,fill=group))+
-  geom_bar(stat="identity", color="black", position=position_dodge(),width=0.7,size=0.25)+
-  scale_fill_manual(values = group_colors) +
-  labs(x="",y="Voting percentage (%)")+
-  theme_manuscript(base_size = figure_font_size)+
-  theme(strip.background = element_blank(),
-        panel.grid.major.x  = element_blank(),
-        axis.line = element_blank(),
-        strip.text = element_text(size = figure_font_size),
-        panel.border = element_rect(colour = "black", fill=NA, size=0.8),
-        plot.margin = unit(c(0.5,0,0.2,1), "lines"),
-        legend.position = "top")+
-  coord_flip()+
-  scale_y_continuous(limits = c(0, 10)) # 设置X轴范围为0到100
-All_feature
-ggsave(All_feature, file = "E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/All_feature_rank.pdf", width = 6, height = 5)
+df=df[order(df$Modality,-df$mean_imp),]
+df$Feature <- factor(df$Feature, levels = rev(unique(df$Feature)))
 
-#DHP
-DHP$feat_name = factor(DHP$feat_name, levels = c(
-  "RNA_Th2 cells","Prot_RPL19","Prot_CDK12","Prot_MIEN1","Prot_ERBB2_PG",
-  "RNA_Glutathione_metabolism","RNA_mRNA-PGR","Prot_ERBB2","RNA_mRNA-ERBB2","RNA_TAM_M2",
-  "DNA_RPL19_CNA","Prot_EEA1","Prot_FLOT1","Prot_PPFIA1","RNA_Glycolysis","WSI_Cell_Interaction",
-  "DNA_BRCA2_CNA","RNA_Taxane_response","RNA_FCGR3A","Prot_PPP1R1B"
-))
-DHP$feat_name <- fct_rev(DHP$feat_name)
-baseDir <- "E:/Projects/PREDIX_HER2/Multimodal/"
-source (paste0(baseDir,"/Code/theme.R"))
-figure_font_size=13
-DHP_feature=ggplot(data=DHP,aes(feat_name,voting_percentage,fill=group))+
-  geom_bar(stat="identity", color="black", position=position_dodge(),width=0.7,size=0.25)+
-  scale_fill_manual(values = group_colors) +
-  labs(x="",y="Voting percentage (%)")+
-  theme_manuscript(base_size = figure_font_size)+
-  theme(strip.background = element_blank(),
-        panel.grid.major.x  = element_blank(),
-        axis.line = element_blank(),
-        strip.text = element_text(size = figure_font_size),
-        panel.border = element_rect(colour = "black", fill=NA, size=0.8),
-        plot.margin = unit(c(0.5,0,0.2,1), "lines"),
-        legend.position = "top")+
-  coord_flip()+
-  scale_y_continuous(limits = c(0, 12)) 
-DHP_feature
-ggsave(DHP_feature, file = "E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/DHP_feature_rank.pdf", width = 6, height = 5)
+p <- ggplot(df, aes(x = mean_imp, y = Feature, fill = Modality)) +
+  geom_bar(stat = "identity", color = "black", linewidth = 0.5, alpha = 0.9) +
+  scale_fill_manual(values = group_colors) + 
+  labs(x = "Mean Importance", y = "") +     
+  theme_classic() + 
+  theme(
+    axis.text.x = element_text(color = "black"), 
+    axis.text.y = element_text(color = "black"), 
+    legend.position = "top", 
+    legend.title = element_text(face = "bold")
+  )
 
-#T-DM1
-TDM1$feat_name = factor(TDM1$feat_name, levels = c(
-  "DNA_RAB11FIP1_CNA",
-  "RNA_mRNA-PGR",
-  "Prot_PPP1R1B",
-  "Prot_SLC12A2",
-  "DNA_BRCA2_CNA",
-  "RNA_mRNA-ESR1",
-  "DNA_CNV_burden",
-  "RNA_Exosome",
-  "Prot_EEA1",
-  "RNA_Fatty_acid_metabolism",
-  "DNA_coding_mutation_TP53_oncokb",
-  "RNA_FCGR3B",
-  "RNA_ABC_transporter",
-  "Prot_ARL1",
-  "RNA_MHC.I_19272155",
-  "Prot_VAMP3",
-  "RNA_Mast-cells",
-  "WSI_Cell_Interaction",
-  "RNA_HER2DX_luminal",
-  "DNA_PIK3CA_CNA"
-))
-TDM1$feat_name <- fct_rev(TDM1$feat_name)
-baseDir <- "E:/Projects/PREDIX_HER2/Multimodal/"
-source (paste0(baseDir,"/Code/theme.R"))
-figure_font_size=13
-TDM1_feature=ggplot(data=TDM1,aes(feat_name,voting_percentage,fill=group))+
-  geom_bar(stat="identity", color="black", position=position_dodge(),width=0.7,size=0.25)+
-  scale_fill_manual(values = group_colors) +
-  labs(x="",y="Voting percentage (%)")+
-  theme_manuscript(base_size = figure_font_size)+
-  theme(strip.background = element_blank(),
-        panel.grid.major.x  = element_blank(),
-        axis.line = element_blank(),
-        strip.text = element_text(size = figure_font_size),
-        panel.border = element_rect(colour = "black", fill=NA, size=0.8),
-        plot.margin = unit(c(0.5,0,0.2,1), "lines"),
-        legend.position = "top")+
-  coord_flip()+
-  scale_y_continuous(limits = c(0, 8)) 
-TDM1_feature
-ggsave(TDM1_feature, file = "E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/TDM1_feature_rank.pdf", width = 7, height = 5)
 
-# FigS10e-f
+print(p)
+Feature=df$Feature
+ggsave("E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/FigureS11/Imp_Plot.pdf", plot = p, width = 7, height = 10, dpi = 300)
 
-library(data.table);library(ggplot2);library(Polychrome)
-RNA=fread("E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/Best_ML_Results_12_10_25/BestResults/RNA_EXTERNAL_TREAT_0.csv")
 
-rename <- c("fpr","tpr")
-names(RNA)[2:3] <- rename
-RNA[, data := "Transcriptomic Only - ROC AUC: 0.76"]
+library(IOBR);library(data.table);library(tableone)
+df=fread("E:/Projects/PREDIX_HER2/Multimodal/Data/Curated_metrics/clin_multiomics_curated_metrics_PREDIX_HER2.txt")%>%as.data.frame()
+df$Clin_prolifvalu[df$Clin_prolifvalu=="Unknown"]=NA
+df$Clin_prolifvalu=as.numeric(df$Clin_prolifvalu)
+df$DNA_HRD=as.numeric(df$DNA_HRD)
+Feature=setdiff(Feature,c("Clin_ER","Clin_ANYNODES","Clin_TUMSIZE","DNA_coding_mutation_TP53_oncokb","Clin_Arm"))
+str(df[,Feature]) 
+res=batch_wilcoxon(
+  df,
+  target = "pCR",
+  feature =Feature
+)
+res=res[res$p.value<0.05,]
 
-ROC_TDM1 <- ggplot(RNA, aes(y = tpr, x = fpr, colour = data, group = data)) + 
-  geom_line() +
-  theme_bw() + 
-  geom_abline(linetype = "dashed") +
-  ylab("True Positive Rate") +
-  xlab("False Positive Rate") +
-  labs(col = "Feature set") +
-  scale_colour_manual(values = c("#434279", "#f2a104","#72a2c0","#9467bd","#00743f","#d62728")) +
-  theme(legend.position = c(0.725, 0.18))+
-  theme(text = element_text(family = "ArialMT"),
-        panel.grid = element_blank(),
-        axis.text.x = element_text(size = 12),  # 修改 x 轴标签字符大小
-        axis.text.y = element_text(size = 12))
-ROC_TDM1
+df=fread("E:/Projects/PREDIX_HER2/Multimodal/Data/Curated_metrics/clin_multiomics_curated_metrics_PREDIX_HER2.txt")%>%as.data.frame()
+cat=c("Clin_ER","Clin_ANYNODES","Clin_TUMSIZE","DNA_coding_mutation_TP53_oncokb","Clin_Arm")
+tableOne <- CreateTableOne(vars = cat, strata = c("pCR"), data = df,
+                           factorVars = cat)
+print(tableOne)
 
-ggsave(ROC_TDM1, file = "E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/TDM1_roc_curves.pdf", width = 6.2, height = 6.2)
+
+# FigS10c
+
+# FigS10d AUC modality vs arm
+library(data.table);library(ggplotify);library(ggpubr)
+df=fread("E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/Figure7/consensus_performance.csv") 
+df$Model <- factor(df$Model, levels = c("Clin", "DNA", "RNA", "Prot", "WSI","Fused_ElasticNet"))
+
+# 同样，可以固定 Scenario 的颜色顺序
+df$Scenario <- factor(df$Scenario, levels = c("DHP", "T-DM1","Global"))
+
+# --- 2. 使用 ggplot2 绘图 ---
+# --- 2. Plotting ---
+p <- ggplot(df, aes(x = Model, y = Pooled_OOF_AUROC, color = Scenario)) +
+  
+  # Add a horizontal reference line at y = 0.5 (representing a random guess baseline)
+  geom_hline(yintercept = 0.5, linetype = "dashed", color = "grey60", linewidth = 0.8) +
+  
+  # Add error bars for the 95% Confidence Intervals
+  geom_errorbar(aes(ymin = CI_lower_95, ymax = CI_upper_95),
+                position = position_dodge(width = 0.6), # Dodge to prevent overlapping of Scenarios
+                width = 0.2,                            # Width of the error bar caps
+                linewidth = 0.8) + 
+  
+  # Add points for the AUROC mean values
+  geom_point(position = position_dodge(width = 0.6), 
+             size = 3) +
+  
+  # Manually map the specific colors to each Scenario
+  scale_color_manual(values = c("Global" = "#e5c06e", 
+                                "DHP" = "#8491B4FF", 
+                                "T-DM1" = "#91D1C2FF")) +
+  
+  # Set plot title and axis labels
+  labs(title = "Model Performance across Different Modalities",
+       x = "",
+       y = "Pooled OOF AUROC (95% CI)",
+       color = "Scenario") +
+  
+  # Apply theme and customize appearance details
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    
+    # Bold x-axis text and rotate 45 degrees to prevent long names from overlapping
+    axis.text.x = element_text(size = 11, face = "bold", angle = 45, hjust = 1),  
+    axis.text.y = element_text(size = 10),
+    
+    # Remove minor grid lines for a cleaner look
+    panel.grid.minor = element_blank(),                    
+    
+    # Add faint vertical grid lines to separate modalities clearly
+    panel.grid.major.x = element_line(linetype = "dotted", color = "grey80"),
+    
+    # Position the legend at the top
+    legend.position = "top", 
+    legend.title = element_text(face = "bold")
+  )
+
+print(p)
+ggsave("E:/Projects/PREDIX_HER2/Multimodal/Figures/Appeal/FigureS11/AUC_Modality_Plot.pdf", plot = p, width = 7, height = 5, dpi = 300)
+
 
 
 
